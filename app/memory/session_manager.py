bc1c61de9e390608310386be4169fb5be5b60c2b
rafi192 <rafihasanmmsc@gmail.com>
2026-05-18 15:02:30 +0600

    Commit of load session in session manager

--- app/memory/session_manager.py
+++ app/memory/session_manager.py
@@ -10,3 +10,29 @@ from app.schemas.session_schema import UserSession, empty_session
 
 logger = logging.getLogger(__name__)
 
+class SessionManager:
+
+    def __init__(self, redis_client: redis.Redis):
+        self.redis = redis_client
+        self.ttl = settings.REDIS_TTL_SECONDS
+
+    
+    #load session from redis. 
+    #if the key doesnt exist( new user or expired TTL), 
+    #create a fresh empty session and return it
+    async def load_session(self, user_id:str) -> UserSession:
+
+        raw_session = await self.redis.get(user_id)
+
+        if raw_session:
+            data = json.loads(raw_session)
+            logger.info(f"session loaded for user_id: {user_id}")
+
+            return UserSession(**data)
+        
+        logger.info(f"no existing session for user_id: {user_id}. creating new session.")
+
+        session = empty_session(user_id)
+        await self.save(session)
+        return session
+
