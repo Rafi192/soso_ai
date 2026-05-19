5dec1585c45c3fb3f208f3cf82ce4dfac369098f
rafi192 <rafihasanmmsc@gmail.com>
2026-05-19 11:27:05 +0600

    Commit of redis client config complete

---
+++ app/memory/redis_client.py
@@ -0,0 +1,28 @@
+#this creates a single shared async redis connection used accross the application
+
+import redis.asyncio as redis
+
+from app.config.settings import settings
+
+redis_client = redis.Redis | None = None
+
+async def get_redis() -> redis.Redis:
+
+    global redis_client
+
+    if redis_client is None:
+        redis_client = redis.Redis(
+            host=settings.REDIS_HOST,
+            port = settings.REDIS_PORT,
+            db= settings.REDIS_DB,
+            decode_responses=True
+        )
+    
+    return redis_client
+
+async def close_redis():
+
+    global redis_client
+    if redis_client:
+        await redis_client.aclose()
+        redis_client = None
